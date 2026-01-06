1fef7c261a697ede5ef5bebddc3b096ea7b38aaa
Yaghs <syaghinloo@gmail.com>
2026-01-06 18:02:28 -0500

    created a main component
    added an input field and a button JSX
    
    added styling to the main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header.js"
+import Main from "./components/Main.js"
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
-      <h1>Hello world</h1>
-    </div>
+      <Main />
+    </>
   );
 }
 
