c6d40133c11d04f34ca6d9de9c4b4064b1a09d9e
Yaghs <syaghinloo@gmail.com>
2026-01-07 17:53:53 -0500

    Implement ingredient addition functionality with form handling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,42 @@
+import {useState} from "react"
 export default function Main(){
+    const [ingredients, setIngredients] = useState([])
+    const ingredientsList = ingredients.map((ingredient)=>{
+        return (
+            <li key={ingredient}>{ingredient}</li>
+        )
+    })
+
+    function handleClick(){
+        console.log("button clicked");
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        console.log("Form data before: " , formData)
+        const newIngredient = formData.get("ingredient")
+        console.log("Form data after:", newIngredient)
+        if(newIngredient && newIngredient !== ""){
+          setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+        }
+        console.log(ingredients)  ;
+    }
     return(
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    name="ingredient"
                     type="text"
                     placeholder = "e.g oregano"
                     aria-label="Add ingredient"
                 />
-                <button>Add ingredient</button>
+                <button onClick={handleClick}>Add ingredient</button>
+                
             </form>
+                <ul>
+                    {ingredientsList}
+                </ul>
         </main>
     )
 }
